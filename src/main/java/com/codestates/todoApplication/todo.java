5b5bf5c838cdec17f63a4468a7c555fc8aa309c6
song4529 <gusdn4529@gmail.com>
2023-06-12 16:43:30 +0900

    String 수정

--- src/main/java/com/codestates/todoApplication/todo.java
+++ src/main/java/com/codestates/todoApplication/todo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class todo {
     @GetMapping
-    public String hellowWorld() {
+    public String todoMessage() {
         return "To-do Application!";
     }
 }
